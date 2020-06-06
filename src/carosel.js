5d789490ab77db5914f1ce50b6d9d5302f87bea1
Netta90 <antobrg90@hotmail.it>
2020-06-06 11:53:03 +0200

    new

--- src/carosel.js
+++ src/carosel.js
@@ -3,21 +3,18 @@ import Prima from './prima.png';
 import Seconda from './seconda.png';
 import Terza from './terza.png';
 import Carousel from 'react-bootstrap/Carousel';
-
+import './carousel.css';
+   
 class Carosel extends React.Component {
 	render() {
 		return(
 			<Carousel>
-			  <Carousel.Item>
+			  <Carousel.Item className = {}>
 			    <img
 			      className="d-block w-100"
 			      src={Prima}
 			      alt="First slide"
 			    />
-		       <Carousel.Caption>
-			      <h3>First slide label</h3>
-			      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-			    </Carousel.Caption>
 			  </Carousel.Item>
 			  <Carousel.Item>
 			    <img
@@ -26,21 +23,24 @@ class Carosel extends React.Component {
 			      alt="Third slide"
 			    />
 
-			    <Carousel.Caption>
-			      <h3>Second slide label</h3>
-			      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-			    </Carousel.Caption>
+			   
 			  </Carousel.Item>
 			  <Carousel.Item>
 			    <img
 			      className="d-block w-100"
 			      src={Terza}
-			      alt="Third slide"
+				  alt="Third slide"
 			    />
+				 <Carousel.Item>
+				 <label>
+					<h3>Vivi libero da restrizioni</h3>
+				</label>
+				</Carousel.Item>
 
 			    <Carousel.Caption>
-			      <h3>Third slide label</h3>
-			      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+				<label>
+					<h3>Vivi libero da restrizioni</h3>
+				</label>
 			    </Carousel.Caption>
 			  </Carousel.Item>
 			</Carousel>
