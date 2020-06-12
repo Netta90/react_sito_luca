535b48ada1f97b4650259b4462f27025c1aace21
Netta90 <antobrg90@hotmail.it>
2020-06-12 12:04:01 +0200

    New

--- src/carosel.js
+++ src/carosel.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Prima from './prima.png';
+import Prima from './Luca.png';
 import Seconda from './seconda.png';
 import Terza from './terza.png';
 import Carousel from 'react-bootstrap/Carousel';
@@ -8,56 +8,57 @@ import './carousel.css';
 
 class Carosel extends React.Component {
 	render() {
-		return(
+		return (
 			<Carousel>
 
-			  <Carousel.Item>
-			    <div			      
-			    className="d-block w-100 FotoLuca"
-			      alt="First slide">
+				<Carousel.Item>
+					<img
+						className="d-block w-100"
+						src={Prima}
+						alt="Third slide"
+					/>
+					<Carousel.Caption className="message prima">
+						<div className="borderbuttom">Falchi Luca </div>
+						<div>PNL Coaching </div>
 
-		       <Carousel.Caption className="message prima">
-			     <div className="borderbuttom">Falchi Luca </div>
-			     <div>PNL Coaching </div>
 
-	        		
-		    			
-			    </Carousel.Caption>
-			   </div>
 
-			  </Carousel.Item>
+					</Carousel.Caption>
 
-			  <Carousel.Item>
-			    <img
-			      className="d-block w-100"
-			      src={Seconda}
-			      alt="Third slide"
-			    />
-			    <Carousel.Caption>
-			      <h3 className="message seconda">Vivi libero da restrizioni</h3>
-			    </Carousel.Caption>
-			  </Carousel.Item>
-			  <Carousel.Item>
-			    <img
-			      className="d-block w-100"
-			      src={Terza}
-				  alt="Third slide"
-			    />
-				 <Carousel.Item>
-				 <label>
-					<h3>Vivi libero da restrizioni</h3>
-				</label>
+
 				</Carousel.Item>
 
-			    <Carousel.Caption>
+				<Carousel.Item>
+					<img
+						className="d-block w-100"
+						src={Seconda}
+						alt="Third slide"
+					/>
+					<Carousel.Caption>
+						<h3 className="message seconda">Vivi libero da restrizioni</h3>
+					</Carousel.Caption>
+				</Carousel.Item>
+				<Carousel.Item>
+					<img
+						className="d-block w-100"
+						src={Terza}
+						alt="Third slide"
+					/>
+					<Carousel.Item>
+						<label>
+							<h3>Vivi libero da restrizioni</h3>
+						</label>
+					</Carousel.Item>
 
-				 	<h3 className="message terza">I risultati sono come un eco:
+					<Carousel.Caption>
+
+						<h3 className="message terza">I risultati sono come un eco:
 	    			Se non ti piace quello che arriva devi cambiare quello che invii.</h3>
 
-			    </Carousel.Caption>
-			  </Carousel.Item>
+					</Carousel.Caption>
+				</Carousel.Item>
 			</Carousel>
-			
+
 
 		);
 	}
--- src/content.js
+++ src/content.js
@@ -7,37 +7,85 @@ import Image from 'react-bootstrap/Image';
 
 class Content extends React.Component {
 
-    render() {
-        return (
-        	
-        	<div>
-	        	<h2 className='title first'>Prima di tutto, mi presento:</h2>
+	render() {
+		return (
+
+			<div>
+				<h2 className='title first'>Prima di tutto, mi presento:</h2>
 				<h3 className='title'>Sono Falchi Luca sono un Licensed NLP Coach e Master Pratictioner.</h3>
 				<p className='and'>Ovvero?</p>
-			<section className="content name">
-               <p>Un modo un po’ macchinoso per dire che le mie competenze rientrano nel campo dello sviluppo personale.
-				Chi si rivolge a me avrà un’ aiuto concreto nel raggiungere i propri obbiettivi.
-				Quante volte vi è capitato di pensare:” Oh vorrei tanto raggiungere quell’obbilettivo”?
+				<section className="content name">
+					<p>Un modo un po’ macchinoso per dire che le mie competenze rientrano nel campo dello sviluppo personale.
+					Chi si rivolge a me avrà un’ aiuto concreto nel raggiungere i propri obbiettivi.
+					Quante volte vi è capitato di pensare:” Oh vorrei tanto raggiungere quell’obbilettivo”?
 				Quante ancora vi demoralizzate o peggio vi autosabotate con scuse inutili che per qualche motivo non riuscite a contrastare?</p>
-			
-				<p>
-	            Bhe qui entro in gioco io, il tuo personal Coach!!
-				Il tutto con metodi, tecniche e una comunicazione positiva inconscia, che porterà la vostra mente ad aprirsi alle nuove possibilità,a liberarsi dalle catene invisibili che la mente vi impone e ad aprirvi ai nuovi orizzonti, scoprirete cosi di essere capaci di cose straordinarie.
+
+					<p>
+						Bhe qui entro in gioco io, il tuo personal Coach!!
+						Il tutto con metodi, tecniche e una comunicazione positiva inconscia, che porterà la vostra mente ad aprirsi alle nuove possibilità,a liberarsi dalle catene invisibili che la mente vi impone e ad aprirvi ai nuovi orizzonti, scoprirete cosi di essere capaci di cose straordinarie.
 				</p>
-            </section>
-            <section className="content name">
-               <p>
-            	il mio lavoro consiste in farti ottenere tutto questo.
-				In Quasi Dieci anni di esperienza in questo settore ho visto Le persone che si sono affidate alle mie consulenze prendere in mano la loro vita, esattamente come ho fatto io a suo tempo, e farne qualcosa di meraviglioso.
+					<p>
+						il mio lavoro consiste in farti ottenere tutto questo.
+						In Quasi Dieci anni di esperienza in questo settore ho visto Le persone che si sono affidate alle mie consulenze prendere in mano la loro vita, esattamente come ho fatto io a suo tempo, e farne qualcosa di meraviglioso.
 
 				</p>
+
+				</section>
+				<hr class="featurette-divider" />
+				<div class="row featurette">
+					<div class="col-md-7">
+						<h2 class="featurette-heading">Che cos'è la Neuro Linguistica? <span class="text-muted">ve lo spiego in breve:
+							</span></h2>
+						<p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod
+						semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus
+						Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod
+						semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus
+						Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod
+						semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus
+						Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod
+						semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus
+						Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod
+						semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus
+						Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod
+						semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus
+
+							</p>
+					</div>
+					<div class="col-md-5">
+						<svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500"
+							height="500" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false"
+							role="img" aria-label="Placeholder: 500x500">
+							<title>Placeholder</title>
+							<rect width="100%" height="100%" fill="#eee"></rect><text x="50%" y="50%" fill="#aaa"
+								dy=".3em">500x500</text>
+						</svg>
+					</div>
+				</div>
+				<hr class="featurette-divider" />
+
+				<div class="row featurette">
+					<div class="col-md-7 order-md-2">
+						<h2 class="featurette-heading">Oh yeah, it’s that good. <span class="text-muted">See for yourself.</span>
+						</h2>
+						<p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod
+						semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus
+             				</p>
+					</div>
+					<div class="col-md-5 order-md-1">
+						<svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500"
+							height="500" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false"
+							role="img" aria-label="Placeholder: 500x500">
+							<title>Placeholder</title>
+							<rect width="100%" height="100%" fill="#eee"></rect><text x="50%" y="50%" fill="#aaa"
+								dy=".3em">500x500</text>
+						</svg>
+					</div>
+				</div>
 				
 
-            </section>
-
-            </div>
-        );
-    }
+			</div>
+		);
+	}
 }
 
 export default Content;
