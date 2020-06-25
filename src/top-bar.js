209037a7bbba8b31b137fea371350830832986c3
Netta90 <antobrg90@hotmail.it>
2020-06-25 09:36:10 +0200

    fine barra

--- src/top-bar.js
+++ src/top-bar.js
@@ -13,26 +13,18 @@ class TopBar extends React.Component {
 
 						<div className="header-containerinte-clearfix">
 							<div className="logo">
-								<div className="logo-iner"><img  className="logo"src={Logo}/>
+								<div className="logo-iner"><img className="logo" src={Logo} />
 								</div>
 							</div>
 							<div className="second-header">
 								<div className="main-menu">
 									<ul className="menu-principale">
-										<li className="menu"> 
+										<li className="menu">
 											<a>Home</a></li>
 										<li className="menu">
-											<a>Offerte</a></li>
-										<li className="menu">
-											<a>Tour</a></li>
-										<li className="menu">
-											<a>Incoming</a></li>
-										<li className="menu">
-											<a>Servizi</a></li>
+											<a>PNL</a></li>
 										<li className="menu">
 											<a>Contatti</a></li>
-										<li className="menu">
-											<a>Preventivi</a></li>
 									</ul>
 									<div className="slide-bar"></div>
 								</div>
