24c93e8b5f077d2a1d8a4601cb0b6ebf3b37904c
Netta90 <antobrg90@hotmail.it>
2020-07-04 15:19:41 +0200

    nuove

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import TopBar from './top-bar';
-import TopbarNew from './TopbarNew';
+import TopbarNew from './top-bar';
 import Carosel from './carosel';
 import Content from './content';
 import Footer from './Footer';
--- src/top-bar.js
+++ src/top-bar.js
@@ -1,38 +1,27 @@
 import React from 'react';
 import './Topbar.css';
-import Logo from './FF.png';
+import Logo from './NuovoLucs.png';
 
 
 class TopBar extends React.Component {
 	render() {
 		return (
-			<div className="header-backround-trasparent">
-				<header className="color-header">
-					<div className="header-backround"></div>
-					<div className="header-container">
+			<div className=" navbar navbar-expand-md navbar-dark fixed-top bg-dark">
 
-						<div className="header-containerinte-clearfix">
-							<div className="logo">
-								<div className="logo-iner"><img className="logo" src={Logo} />
-								</div>
-							</div>
-							<div className="second-header">
-								<div className="main-menu">
-									<ul className="menu-principale">
-										<li className="menu">
-											<a>Home</a></li>
-										<li className="menu">
-											<a>PNL</a></li>
-										<li className="menu">
-											<a>Contatti</a></li>
-									</ul>
-									<div className="slide-bar"></div>
-								</div>
-							</div>
-
-						</div>
-					</div>
-				</header>
+				<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+					<image className="logo" src={Logo} />
+					<span className="navbar-toggler-icon"></span>
+				</button>
+				<div className="collapse navbar-collapse" id="navbarCollapse">
+					<ul className="navbar-nav mr-auto">
+						<li className="nav-item active">
+							<a className="nav-link" href="#">Home</a>
+						</li>
+						<li className="nav-item">
+							<a className="nav-link" href="#">Contatti</a>
+						</li>
+					</ul>
+				</div>
 			</div>
 		);
 	}
