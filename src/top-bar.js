1496ec37df807e3fde1800168bc1248eab761945
Netta90 <antobrg90@hotmail.it>
2020-07-09 11:44:10 +0200

    Nuova versione dela barra

--- src/top-bar.js
+++ src/top-bar.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import './Topbar.css';
 import Logo from './NuovoLucs.png';
+import './top-bar.css';
+import './TopbarNew.css';
 
 
 class TopBar extends React.Component {
 	render() {
 		return (
-			<div className=" navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-
+			<div className=" navbar navbar-expand-md navbar-dark fixed-top bg-dark-new">
+				<div className="box-logo">
+					<div className="misure-box">
+						<img className="logo " src={Logo} />
+					</div>
+				</div>
 				<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-					<image className="logo" src={Logo} />
 					<span className="navbar-toggler-icon"></span>
 				</button>
 				<div className="collapse navbar-collapse" id="navbarCollapse">
@@ -20,6 +24,13 @@ class TopBar extends React.Component {
 						<li className="nav-item">
 							<a className="nav-link" href="#">Contatti</a>
 						</li>
+						<li className="nav-item">
+							<a className="nav-link" href="#">Contatti</a>
+						</li>
+						<li className="nav-item">
+							<a className="nav-link" href="#">Contatti</a>
+						</li>
+
 					</ul>
 				</div>
 			</div>
