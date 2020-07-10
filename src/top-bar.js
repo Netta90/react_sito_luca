bbfe297329ad73dd7713c567acd5b38d3413f204
Netta90 <antobrg90@hotmail.it>
2020-07-10 12:13:09 +0200

    sistemazione barra

--- src/top-bar.js
+++ src/top-bar.js
@@ -14,7 +14,10 @@ class TopBar extends React.Component {
 				</div>
 
 				<div className="border-boxTwo">
-					<div className="collapse navbar-collapse" id="navbarCollapse">
+					<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="true" aria-label="Toggle navigation">
+						<span className="navbar-toggler-icon"></span>
+					</button>
+					<div className="navbar-collapse collapse show" id="navbarCollapse">
 						<ul className="navbar-nav mr-auto">
 							<li className="nav-item active">
 								<a className="nav-link" href="#">Home</a>
